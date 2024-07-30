1f5501c94a65e6e8247b43e88e084ca47e9b4a31
최수현 <sw92522@naver.com>
2024-07-30 14:42:00 +0900

    0730 최수현

---
+++ 20240730/suhyun/19638_센티와_마법의_뿅망치.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer input = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(input.nextToken());
+		int H = Integer.parseInt(input.nextToken());
+		int T = Integer.parseInt(input.nextToken());
+		double one = 1;
+		
+    // 나누기 2를 해야하기 때문에 double로 타입을 지정했습니다.
+		PriorityQueue<Double> people = new PriorityQueue<>(Collections.reverseOrder());
+		
+		for (int i = 0; i < N; i++) {
+			people.add(Double.parseDouble(br.readLine()));
+		}
+		
+		int cnt = 0;
+		
+		for (int t = 0; t < T; t++) {
+			double height = people.poll();
+			
+			if (height < H) {
+				System.out.println("YES\n" + cnt);
+				return;
+			}
+			
+			if (height <= 1) {
+				people.add(height);
+			} else {
+				people.add(height/2);
+			}
+			
+			cnt++;
+		}
+		
+		double biggest = people.poll();
+		
+		if (biggest < H) {
+			System.out.println("YES\n" + cnt);
+		} else {
+			System.out.println("NO\n" + (int) biggest);
+		}
+		
+	}
+}
