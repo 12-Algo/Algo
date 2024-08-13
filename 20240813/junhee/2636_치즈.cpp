820601aa4ecebe5c0653ad0b49f5a0548647743d
JunHee <ljh19980823@gmail.com>
2024-08-13 10:00:48 +0900

    0813 이준희

---
+++ 20240813/junhee/2636_치즈.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <climits>
+#include <string>
+
+using namespace std;
+
+int n, m;
+int map[101][101] = { 0, };
+int dx[4] = { 0,1,0,-1 };
+int dy[4] = { 1,0,-1,0 };
+
+struct cheeze {
+	int x, y, depth;
+};
+
+int check_cheeze() {
+	int cnt = 0;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			if (map[i][j] == 1)
+				cnt++;
+		}
+	}
+	return cnt;
+}
+
+void bfs() {
+	queue<cheeze> q;
+	bool visit[101][101] = { false, };
+	q.push({ 0,0,0 });
+	while (!q.empty()) {
+		cheeze now = q.front();
+		q.pop();
+		for (int i = 0; i < 4; i++) {
+			int nx = now.x + dx[i];
+			int ny = now.y + dy[i];
+			if (0 <= nx && nx < n && 0 <= ny && ny < m) {
+				if (visit[nx][ny] == false && now.depth == 0) {
+					if (map[nx][ny] == 0) {
+						visit[nx][ny] = true;
+						q.push({ nx,ny,0 });
+					}
+					else {
+						visit[nx][ny] = true;
+						map[nx][ny] = 0;
+						q.push({ nx,ny,1 });
+						
+					}
+				}
+			}
+		}
+	}
+}
+
+int main() {
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			int temp;
+			cin >> temp;
+			map[i][j] = temp;
+		}
+	}
+	int prev = 0;
+	int answer = 0;
+	while (check_cheeze() != 0) {
+		prev = check_cheeze();
+		bfs();
+		answer++;
+	}
+	cout << answer << endl << prev;
+	return 0;
+}
